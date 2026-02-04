a1660ac549a4c330bffdf6378656bf4cba29f343
DineshRana716 <dkr9304650@gmail.com>
2026-02-05 00:01:00 +0530

    added the logout functionality

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import style from "./Header.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
   return (
     <header className={style.header}>
       <div className={style.leftSection}>
@@ -13,7 +20,11 @@ const Header = () => {
         </button>
       </div>
       <div className={style.rightSection}>
-        <button type="button" className={style.actionBtn}>
+        <button
+          type="button"
+          className={style.actionBtn}
+          onClick={handleLogout}
+        >
           Logout
         </button>
         <span className={style.appName}>MYvault</span>
